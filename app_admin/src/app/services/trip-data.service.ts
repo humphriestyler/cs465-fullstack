2bc80477b0c73ea97f489e13885e4a657caad849
tyler <th28591@email.vccs.edu>
2023-12-07 00:41:20 -0500

    adding add trip, fixing images (it was seedgoose!)

--- app_admin/src/app/services/trip-data.service.ts
+++ app_admin/src/app/services/trip-data.service.ts
@@ -9,6 +9,16 @@ export class TripDataService {
   constructor(private http: Http) { }
 
   private apiBaseUrl = 'http://localhost:3000/api/';
+  private tripUrl = `${this.apiBaseUrl}trips/`;
+
+  public addTrip(formData: Trip): Promise<Trip> {
+    console.log('Inside TripDataService#addTrip');
+    return this.http
+      .post(this.tripUrl, formData)
+      .toPromise()
+      .then(response => response.json() as Trip[])
+      .catch(this.handleError);
+  }
 
   public getTrips(): Promise<Trip[]> {
     console.log('Inside TripDataService#getTrips');
