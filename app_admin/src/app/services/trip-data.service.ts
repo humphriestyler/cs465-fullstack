f22df003c332a2c204f9c80dd99472446c7ce9a9
tyler <th28591@email.vccs.edu>
2023-12-07 01:13:26 -0500

    adding update trip

--- app_admin/src/app/services/trip-data.service.ts
+++ app_admin/src/app/services/trip-data.service.ts
@@ -29,6 +29,24 @@ export class TripDataService {
       .catch(this.handleError);
   }
 
+  public getTrip(tripCode: string): Promise<Trip> {
+    console.log('Inside TripDataService#getTrip(tripCode)');
+    return this.http
+    .get(this.tripUrl + tripCode)
+    .toPromise()
+    .then(response => response.json() as Trip)
+    .catch(this.handleError);
+    }
+    public updateTrip(formData: Trip): Promise<Trip> {
+    console.log('Inside TripDataService#upateTrip');
+    console.log(formData);
+    return this.http
+    .put(this.tripUrl + formData.code, formData)
+    .toPromise()
+    .then(response => response.json() as Trip[])
+    .catch(this.handleError);
+    }
+
   private handleError(error: any): Promise<any> {
     console.error('Something has gone wrong', error); // for demo purposes only
     return Promise.reject(error.message || error);
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -5,7 +5,9 @@ const tripsController = require('../controllers/trips');
 
 router
     .route('/trips')
-    .get(tripsController.tripsList);
+    .get(tripsController.tripsList)
+    .post(tripsController.tripsAddTrip)
+    .put(tripsController.tripsUpdateTrip);
 
 router
     .route('/trips/:tripCode')
